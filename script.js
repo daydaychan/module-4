95a9ed472c9cd34c59f0070aa0a4ca8496702421
daydaychan <67956388+daydaychan@users.noreply.github.com>
2021-07-28 16:23:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="Joe";
-names[1]="Tim";
-names[2]="Jimmy";
-names[3]="Jane";
-names[4]="Andy";
-names[5]="Owen";
-names[6]="Darren";
-names[7]="Marcus";
-names[8]="May";
-names[9]="Shirley";
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 
 for (var i = 0; i < names.length; i++) {
